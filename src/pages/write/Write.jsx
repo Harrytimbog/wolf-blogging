8303b69372cf6c6848fc75785142b641346f1bb3
Timilehin Arigbede <Harriemannie@gmail.com>
2022-02-22 15:07:45 +0100

    create a new post component

---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,37 @@
+import './write.css';
+
+function Write() {
+  return (
+    <div className="write">
+      <img
+        className="writeImg"
+        src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+        alt=""
+      />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fa-solid fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: 'none' }} />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Tell your story..."
+            type="text"
+            className="writeInput writeText"
+          ></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
+    </div>
+  );
+}
+
+export default Write;
