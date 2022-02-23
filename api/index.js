705bf9bfc7f6aa97a4c8677e3ea6a5ca93e15492
Timilehin Arigbede <Harriemannie@gmail.com>
2022-02-23 16:31:00 +0100

    seperating api from client, setting up express server and also connecting mongo DB

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
 
-app.use('/', (req, res) => {
-  console.log(`Hey this is main url`);
-});
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err));
 
 app.listen('5000', () => {
   console.log('Backend is running');
