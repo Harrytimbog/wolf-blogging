5fde8146ac3d62a48a0a216833dfb9d2d933e903
Timilehin Arigbede <Harriemannie@gmail.com>
2022-02-28 16:19:22 +0100

    React Context API best Practices and complete the login system

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,25 +1,56 @@
+import { useContext } from 'react';
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import './login.css';
+import { Context } from '../../context/Context';
+import axios from 'axios';
 
 export default function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { user, dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({
+      type: 'LOGIN_START',
+    });
+    try {
+      const res = await axios.post('/auth/login', {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+
+      dispatch({ type: 'LOGIN_SUCCESS', payload: res.data });
+    } catch (error) {
+      dispatch({ type: 'LOGIN_FAILURE' });
+    }
+  };
+
+  console.log(user);
+
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
       <span className="loginTitle"></span>
-      <form className="loginForm">
-        <label>Email</label>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
         <input
           className="loginInput"
           type="text"
-          placeholder="Enter your email..."
+          placeholder="Enter your username..."
+          ref={userRef}
         />
         <label>Password</label>
         <input
           className="loginInput"
           type="password"
           placeholder="Enter your password..."
+          ref={passwordRef}
         />
-        <button className="loginButton">Login</button>
+        <button className="loginButton" type="submit">
+          Login
+        </button>
       </form>
       <button className="loginRegisterButton">
         <Link className="link" to="/register">
