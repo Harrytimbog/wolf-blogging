17a815b493fe81607134b57a44255156cf00c4ea
Timilehin Arigbede <Harriemannie@gmail.com>
2022-02-28 12:21:37 +0100

    Displaying all posts on home page

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,27 +1,24 @@
 import './post.css';
 
-export default function Post() {
+export default function Post({ post }) {
   return (
     <div className="post">
-      <img className="postImg" src="/images/beach.jpg" alt="" />
+      {post.photo && <img className="postImg" src={post.photo} alt="" />}
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">Music</span>
-          <span className="postCat">Life</span>
+          <span className="postCat">
+            {post.categories.map((c) => (
+              <span className="postCat">{c.name}</span>
+            ))}
+          </span>
         </div>
-        <span className="postTitle">Lorem Ipsum is simply dummy text</span>
+        <span className="postTitle">{post.title}</span>
         <hr />
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-      <p className="postDesc">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+      <p className="postDesc">{post.description}</p>
     </div>
   );
 }
